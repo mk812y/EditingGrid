6f0f9d10a904dd54087ec2577a4ecfc4f8db0077
mk812y <mk812y@gmail.com>
2023-01-22 18:18:54 +0300

    Initial Commit

---
+++ EditingGrid/EditingGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditingGridApp.swift
+//  EditingGrid
+//
+//  Created by Михаил Куприянов on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct EditingGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
